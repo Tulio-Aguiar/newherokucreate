8561197ad88bd65e6de838a5f807742488fec1f5
Tulio-Aguiar <axeaguiar@hotmail.com>
2021-10-07 09:29:31 +0200

    Project structure

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port=3000;
+
+app.set('views', './views')
+
+app.listen(port,()=>{
+    console.log(`listening at http://localhost:${port}`)
+})
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+app.get('/', (req, res) => {
+    res.render('about.ejs')
+})
